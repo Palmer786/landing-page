d05f164b84da14fa39d83472b5906d62f3a4b66e
Palmer786 <palmer786786@gmail.com>
2020-07-27 13:47:24 +0200

    feature: Add new production section to page

---
+++ src/components/ProductionSection/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import productionProject from '../../images/production-project.png';
+import searchIcon from '../../images/search-icon.png';
+
+const MainWrapper = styled.section`
+  width: 80%;
+  height: 1010px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const ContentBox = styled.div`
+  width: 50%;
+  height: 210px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const GreenCircle = styled.div`
+  height: 48px;
+  width: 48px;
+  background: #8cd31a;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  font-size: 32px;
+  font-weight: bold;
+  color: #304156;
+  text-align: center;
+`;
+
+const SubTitle = styled.h2`
+  font-weight: bold;
+  color: #304156;
+  text-align: center;
+  font-size: 18px;
+  line-height: 1.7;
+`;
+
+export default () => (
+  <MainWrapper>
+    <ContentBox>
+      <GreenCircle>
+        <img src={searchIcon} alt="icon" />
+      </GreenCircle>
+      <Title>Closer Look at Items</Title>
+      <SubTitle>
+        The moment you think of buying a Web Hosting Plan, you know one thing.
+        So many choices, which one to choose?The moment you think of buying a
+        Web Hosting Plan, you know one thing.
+      </SubTitle>
+    </ContentBox>
+    <img src={productionProject} alt="project" />
+  </MainWrapper>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,14 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import TopBar from '../components/TopBar';
 import Header from '../components/Header';
+import ProductionSection from '../components/ProductionSection';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Landing page" />
     <TopBar />
     <Header />
+    <ProductionSection />
   </Layout>
 );
 
