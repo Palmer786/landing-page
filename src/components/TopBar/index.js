28f5b56f3d8f0e74b95fc1afdc0c828883a422d5
Palmer786 <palmer786786@gmail.com>
2020-07-27 13:19:13 +0200

    refactor: Move TopBar from main page to new component

---
+++ src/components/TopBar/index.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Link} from 'gatsby';
+
+import logoImage from '../../images/logo.png';
+
+const TopBarWrapper = styled.div`
+  width: 100%;
+  height: 154px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const TopBar = styled.nav`
+  width: 96%;
+  height: 32px;
+  margin: 17px auto 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LogoBox = styled.div`
+  width: 10%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const NavBox = styled.div`
+  width: 30%;
+  height: 100%;
+  display: flex;
+`;
+
+const ListWrapper = styled.ul`
+  width: 80%;
+  height: 100%;
+  list-style: none;
+  display: flex;
+`;
+
+const ListElement = styled.li`
+  width: 20%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-size: 13px;
+  font-weight: 700;
+  color: white;
+`;
+
+const SignInButton = styled.button`
+  width: 20%;
+  height: 100%;
+  background: white;
+  border: none;
+  border-radius: 4px;
+  color: #304156;
+  font-size: 15px;
+  cursor: pointer;
+`;
+
+export default () => (
+  <TopBarWrapper>
+    <TopBar>
+      <LogoBox>
+        <img src={logoImage} alt="logo" />
+      </LogoBox>
+      <NavBox>
+        <ListWrapper>
+          <ListElement>
+            <StyledLink to="#">First</StyledLink>
+          </ListElement>
+          <ListElement>
+            <StyledLink to="#">Second</StyledLink>
+          </ListElement>
+          <ListElement>
+            <StyledLink to="#">Third</StyledLink>
+          </ListElement>
+          <ListElement>
+            <StyledLink to="#">Fourth</StyledLink>
+          </ListElement>
+          <ListElement>
+            <StyledLink to="#">Firth</StyledLink>
+          </ListElement>
+        </ListWrapper>
+        <SignInButton>Sign In</SignInButton>
+      </NavBox>
+    </TopBar>
+  </TopBarWrapper>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,76 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Link} from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import logoImage from '../images/logo.png';
+import TopBar from '../components/TopBar'
 import iphoneMock from '../images/iphonemock.png';
 import clockIcon from '../images/clock-icon.png';
 import boxIcon from '../images/box-icon.png';
 
-const TopBarWrapper = styled.div`
-  width: 100%;
-  height: 154px;
-  display: flex;
-  flex-direction: column;
-`;
-
-const TopBar = styled.nav`
-  width: 96%;
-  height: 32px;
-  margin: 17px auto 0 auto;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const LogoBox = styled.div`
-  width: 10%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const NavBox = styled.div`
-  width: 30%;
-  height: 100%;
-  display: flex;
-`;
-
-const ListWrapper = styled.ul`
-  width: 80%;
-  height: 100%;
-  list-style: none;
-  display: flex;
-`;
-
-const ListElement = styled.li`
-  width: 20%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  font-size: 13px;
-  font-weight: 700;
-  color: white;
-`;
-
-const SignInButton = styled.button`
-  width: 20%;
-  height: 100%;
-  background: white;
-  border: none;
-  border-radius: 4px;
-  color: #304156;
-  font-size: 15px;
-  cursor: pointer;
-`;
 
 const HeaderWrapper = styled.header`
   width: 100%;
@@ -111,7 +48,7 @@ const FeatureText = styled.p`
 
 const FeatureListBox = styled.div`
   width: 100%;
-  height: 121px;
+  height: 250px;
   border-top: 1px solid white;
   display: flex;
   flex-direction: column;
@@ -158,33 +95,7 @@ const FeatureButton = styled.button`
 const IndexPage = () => (
   <Layout>
     <SEO title="Landing page" />
-    <TopBarWrapper>
-      <TopBar>
-        <LogoBox>
-          <img src={logoImage} alt="logo" />
-        </LogoBox>
-        <NavBox>
-          <ListWrapper>
-            <ListElement>
-              <StyledLink to="#">First</StyledLink>
-            </ListElement>
-            <ListElement>
-              <StyledLink to="#">Second</StyledLink>
-            </ListElement>
-            <ListElement>
-              <StyledLink to="#">Third</StyledLink>
-            </ListElement>
-            <ListElement>
-              <StyledLink to="#">Fourth</StyledLink>
-            </ListElement>
-            <ListElement>
-              <StyledLink to="#">Firth</StyledLink>
-            </ListElement>
-          </ListWrapper>
-          <SignInButton>Sign In</SignInButton>
-        </NavBox>
-      </TopBar>
-    </TopBarWrapper>
+  <TopBar />
     <HeaderWrapper>
       <HeaderBox>
         <FeatureBox>
@@ -208,8 +119,8 @@ const IndexPage = () => (
                 Whether you would want
               </FeatureListElementLabel>
             </FeatureListElement>
+            <FeatureButton>Button Name</FeatureButton>
           </FeatureListBox>
-          <FeatureButton>Button Name</FeatureButton>
         </FeatureBox>
         <img src={iphoneMock} alt="iphone" />
       </HeaderBox>
